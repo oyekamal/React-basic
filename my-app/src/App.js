5cfcb50f9e19557dc3b7390943b8afd5a35bf462
kamalpay <muhammadkamal@paymob.com>
2023-02-12 23:50:34 +0500

    state added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Textform from './components/Textform';
 
 function App() {
   return (
     <>
     <Navbar title="TextUtile" />
+    <div className="container">
+    <Textform heading="Enter your text below..!" />
+    </div>
+
     </>
   );
 }
---
+++ my-app/src/components/Textform.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+
+export default function Textform(props) {
+    const [text, setText] = useState("Enter text here")
+
+    const UppcaseHandler=()=>{
+        console.log("uppercase");
+        let new_text = text.toUpperCase();
+
+        setText(new_text);
+    }
+
+    const OnChangeHandler=(event)=>{
+        console.log("OnChangeHandler");
+        setText(event.target.value);
+    }
+    return (
+        <div>
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+                <label className="form-label">Example textarea</label>
+                <textarea className="form-control" value={text} onChange={OnChangeHandler} id="mybox" rows="8"></textarea>
+            </div>
+            <button className="btn btn-primary" onClick={UppcaseHandler} >UpperCase</button>
+        </div>
+    )
+}
