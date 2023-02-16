1d3b8e8275d8a1ba5e69f21faaf559bf5884cc40
kamalpay <muhammadkamal@paymob.com>
2023-02-16 12:35:41 +0500

    format data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,10 +5,10 @@ import Alert from './components/Alert';
 import React, { useState } from 'react'
 
 function App() {
-  const [mode, setMode ] = useState('light');
-  const [alert, setAlert ] = useState(null);
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
 
-  const showAlert=(message, type)=>{
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
       type: type
@@ -18,25 +18,25 @@ function App() {
     }, 1500);
   }
   const toggleMode = () => {
-    if (mode === 'light'){
+    if (mode === 'light') {
       setMode('dark');
 
-      showAlert("Dark mode","success");
+      showAlert("Dark mode", "success");
     }
-    else{
+    else {
       setMode('light');
-      showAlert("light mode","success");
+      showAlert("light mode", "success");
     }
   }
 
   // setMode("dark");
   return (
     <>
-    <Navbar title="TextUtile" mode={mode} toggleMode={toggleMode} />
-    <Alert alert={alert}/>
-    <div className="container">
-    <Textform showAlert={showAlert} heading="Enter your text below..!" />
-    </div>
+      <Navbar title="TextUtile" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container">
+        <Textform showAlert={showAlert} heading="Enter your text below..!" />
+      </div>
     </>
   );
 }
